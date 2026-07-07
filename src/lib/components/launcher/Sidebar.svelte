83bfca14718394da8dfc7a40b4233716078d5762
elliesavo <animesabaka1@gmail.com>
2026-07-07 20:57:00 +0300

    Align sidebar hover color with launcher palette

--- src/lib/components/launcher/Sidebar.svelte
+++ src/lib/components/launcher/Sidebar.svelte
@@ -11,10 +11,11 @@
 
 	type Props = {
 		navigation: NavigationItem[];
+		activeSection: SectionId;
 		setActiveSection: (section: SectionId) => void;
 	};
 
-	let { navigation, setActiveSection }: Props = $props();
+	let { navigation, activeSection, setActiveSection }: Props = $props();
 </script>
 
 <aside class="sidebar-surface flex min-h-0 flex-col border-r border-border">
@@ -28,6 +29,7 @@
 		{#each navigation as item}
 			{@const Icon = item.icon}
 			<button
+				class:active={activeSection === item.id}
 				class="nav-button"
 				title={item.label}
 				aria-label={item.label}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -578,7 +578,7 @@
 		{/if}
 
 		<div class:visible={appVisible} class="launcher-layout">
-			<Sidebar {navigation} setActiveSection={openSection} />
+			<Sidebar {navigation} {activeSection} setActiveSection={openSection} />
 
 			<section class="main-surface flex min-w-0 flex-col">
 				<TitleBar
